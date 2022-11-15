dbe5501124b00a4465d90a361f2c2cd5209145a2
aron-helu <aronmilahela@gmail.com>
2022-11-15 22:43:25 +0300

    Authoriziation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :recipe_foods
   resources :recipes
   resources :inventory_foods
   resources :inventories
   resources :foods
-  resources :users
+  # resources :users
   root 'pages#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
