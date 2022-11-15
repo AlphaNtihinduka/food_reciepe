332606e36c69d9f38b03c3badb18ce0c8d00daa9
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-11-15 14:43:11 -0300

    Add has_many and belongs_to relationships

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,6 @@
 class Food < ApplicationRecord
+  has_many :inventoryfoods
+  has_many :inventory, through: :inventoryfoods
+  has_many :recipefoods
+  has_many :recipes, through: :recipefoods
 end
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,5 @@
 class Inventory < ApplicationRecord
+  belongs_to :user
+  has_many :inventoryfoods
+  has_many :foods, through: :inventoryfoods
 end
