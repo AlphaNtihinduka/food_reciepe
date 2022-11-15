9baae2ae1854487d88ecc8a13e2c0c534ad1af7f
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-15 23:19:36 +0300

    Create the home page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :inventories
   resources :foods
   # resources :users
-  root 'pages#hello'
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
