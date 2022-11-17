cce36d0d89404f75cc1a0b38155eebd5adce9326
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-17 10:26:46 +0300

    Validate name in inventory

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,4 +2,6 @@ class Inventory < ApplicationRecord
   belongs_to :user
   has_many :inventoryfoods
   has_many :foods, through: :inventoryfoods
+
+  validates :name, presence: true, length: { maximum: 200 }
 end
