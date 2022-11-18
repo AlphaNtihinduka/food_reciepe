f2579dbd32ecab47e9b177ef9301f4d6bfcad1fb
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-18 20:48:37 +0300

    Add the event listener to button

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const add_btn = document.querySelector(".add_inventory_food_btn")
+const add_inventory_form = document.querySelector(".add_inventory_food_form")
+const create_inventory_food_btn = document.querySelector(".create_inventory_food_btn")
+
+add_btn.addEventListener("click", ()=> {
+    add_inventory_form.style.display = "block"
+  })
+
+  create_inventory_food_btn.addEventListener("click", ()=> {
+    add_inventory_form.style.display = "none"
+    window.location.reload();
+})
