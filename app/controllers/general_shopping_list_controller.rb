c3d43233d2fc46651360c23ad971d27c44f262ba
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-11-19 20:43:16 -0300

    Fix shopping_list controller logic

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,5 +1,23 @@
 class GeneralShoppingListController < ApplicationController
   def index
       @foods = current_user.foods
+      @total = 0
+      @shopping_list = []
+      @total = []
+
+      @foods.each do |food|
+        recipe_foods_quantity = RecipeFood.where(recipe_id: params[:recipe_id]).joins(:food).where(food_id: food.id).sum('quantity')
+        inventory_foods_quantity = InventoryFood.where(inventory_id: params[:inventory_id]).joins(:food).where(food_id: food.id).sum('quantity')
+
+        missing_food = recipe_foods_quantity - inventory_foods_quantity
+
+        if missing_food > 0
+          value = missing_food * food.price
+          #obj = {'name' => "#{food.name}", 'quantity '=> "#{missing_food}", 'value' = "#{value}"}
+          obj = {id: food.id, name: food.name, quantity: missing_food, value: value}
+          @shopping_list << obj
+          @total << obj[:value]
+        end
+      end
   end
 end
