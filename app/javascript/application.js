e5b64792435ff366d9d17f747d334db37b35c6ee
aron-helu <aronmilahela@gmail.com>
2022-11-19 14:34:54 +0300

    Add N+1 queries and style remain parts

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -14,3 +14,17 @@ add_btn.addEventListener("click", ()=> {
     add_inventory_form.style.display = "none"
     window.location.reload();
 })
+
+const add_recipe_btn = document.querySelector(".add_recipe_food_btn")
+// const add_recipe_form = document.querySelector(".add_recipe_food_form")
+// const create_recipe_food_btn = document.querySelector(".create_recipe_food_btn")
+
+add_recipe_btn.addEventListener("click", ()=> {
+    
+    console.log("click")
+  })
+
+//   create_recipe_food_btn.addEventListener("click", ()=> {
+//     add_recipe_form.style.display = "none"
+//     window.location.reload();
+// })
