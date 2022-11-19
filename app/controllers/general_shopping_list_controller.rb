65b1e949d4554355afa5bea206639f3c4c8c9e6c
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-11-19 19:11:20 -0300

    Fix general_shopping_list

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,11 +1,5 @@
 class GeneralShoppingListController < ApplicationController
   def index
-      #@recipe = Recipe.find(params[:recipe_id])
-      #@inventory = Inventory.find_by(id: params[:id])
-      #@shopping_list = missing_foods(@recipe, @inventory)
-  end
-
-  def missing_foods(recipe, inventory)
-
+      @foods = current_user.foods
   end
 end
