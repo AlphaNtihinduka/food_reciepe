3878488bd1fd24752716d944ea4b286b153d55f3
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-11-19 16:14:28 -0300

    Add pop up js and fix bugs

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,5 +1,11 @@
 class GeneralShoppingListController < ApplicationController
   def index
-  
+      @recipe = Recipe.find(params[:recipe_id])
+      @inventory = Inventory.find_by(id: params[:id])
+      #@shopping_list = missing_foods(@recipe, @inventory)
+  end
+
+  def missing_foods(recipe, inventory)
+
   end
 end
