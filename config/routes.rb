b6fecf40e0e08c8c07ac71e026b5fbd470c5f98b
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-11-19 15:02:08 -0300

    Add general shopping list controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :inventories
   resources :foods
   resources :public_recipes
+  resources :genereal_shopping_list
   get 'shopping_list', to: 'foods#shopping_list'
   # resources :users
   root 'pages#home'
